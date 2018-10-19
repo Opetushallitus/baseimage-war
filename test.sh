d7c226600f9de28d63bfcf96c5c31c87bfce7110
Ville Seppänen <ville.seppanen@gofore.com>
2018-10-19 14:48:30 +0300

    List largest directories

--- test.sh
+++ test.sh
@@ -1,14 +1,14 @@
 # Strict mode
 set -eu
 
-echo "Testing that required software is installed"
+echo "Test that required software is installed"
 apk --version
 aws --version
 java -version
 j2 --version
 cat /etc/alpine-release
 
-echo "Testing that baseimage has files expected by the application during run script"
+echo "Test that baseimage has files expected by the application during run script"
 ls -la /opt/tomcat/bin/catalina.sh
 ls -la /opt/tomcat/conf/server.xml
 ls -la /root/oph-configuration/ehcache.xml
@@ -17,5 +17,5 @@ ls -la /opt/tomcat/lib/logback-*
 ls -la /root/jmx_prometheus_javaagent.jar
 ls -la /root/node_exporter
 
-echo "File space usage"
-du -d 2 -h /
+echo "Largest directories:"
+du -d 3 -m /|sort -nr|head -n 20
