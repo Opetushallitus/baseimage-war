068e73f7cfc46824d5f70f576d0095475e26fddc
Ville Seppänen <ville.seppanen@gofore.com>
2018-10-19 14:44:13 +0300

    Add missing test

--- test.sh
+++ test.sh
@@ -11,6 +11,7 @@ cat /etc/alpine-release
 echo "Testing that baseimage has files expected by the application during run script"
 ls -la /opt/tomcat/bin/catalina.sh
 ls -la /opt/tomcat/conf/server.xml
+ls -la /root/oph-configuration/ehcache.xml
 ls -la /opt/tomcat/lib/log4j*
 ls -la /opt/tomcat/lib/logback-*
 ls -la /root/jmx_prometheus_javaagent.jar
