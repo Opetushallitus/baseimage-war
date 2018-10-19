783c281bba5dfa8a346f91bc638c664d8617035c
Ville Seppänen <ville.seppanen@gofore.com>
2018-10-19 14:26:04 +0300

    Add tests

--- test.sh
+++ test.sh
@@ -1,12 +1,20 @@
 # Strict mode
 set -eu
 
-# Tests
+echo "Testing that required software is installed"
 apk --version
 aws --version
 java -version
 j2 --version
 cat /etc/alpine-release
 
-# Show directory sizes
+echo "Testing that baseimage has files expected by the application during run script"
+ls -la /opt/tomcat/bin/catalina.sh
+ls -la /opt/tomcat/conf/server.xml
+ls -la /opt/tomcat/lib/log4j*
+ls -la /opt/tomcat/lib/logback-*
+ls -la /root/jmx_prometheus_javaagent.jar
+ls -la /root/node_exporter
+
+echo "File space usage"
 du -d 2 -h /
