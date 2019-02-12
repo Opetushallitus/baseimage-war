ffe4221f87b6774997b53e3dd1e1611aa2c0494d
August Kilponen <august.kilponen@gmail.com>
2019-02-12 15:01:17 +0200

    Otettu OpenJDK11 käyttöön

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM alpine:latest
+FROM adoptopenjdk/openjdk11:alpine-slim
 
 ARG DL_PATH_TOKEN
 
--- install.sh
+++ install.sh
@@ -66,35 +66,6 @@ echo "9a5c32411a6a06e22b69c495b7975034409fa1652d03aeb8eb5b6f59fd4594e0  /tmp/${J
 wget -c -q -P /tmp/ ${FILES_URL_PREFIX}/${JCE_PACKAGE}
 echo "f3020a3922efd6626c2fff45695d527f34a8020e938a49292561f18ad1320b59  /tmp/${JCE_PACKAGE}" |sha256sum -c
 
-echo "Installing Java JDK"
-mkdir -p /usr/java/latest
-tar xf /tmp/${JDK_PACKAGE} -C /usr/java/latest --strip-components=1
-ln -s /usr/java/latest/bin/* /usr/bin/
-unzip -jo -d /usr/java/latest/jre/lib/security /tmp/${JCE_PACKAGE}
-
-echo "Removing unused JDK sources and libraries"
-rm /usr/java/latest/jre/lib/security/README.txt
-rm -rf /usr/java/latest/*src.zip
-rm -rf /usr/java/latest/lib/missioncontrol
-rm -rf /usr/java/latest/lib/visualvm
-rm -rf /usr/java/latest/lib/*javafx*
-rm -rf /usr/java/latest/jre/lib/plugin.jar
-rm -rf /usr/java/latest/jre/lib/ext/jfxrt.jar
-rm -rf /usr/java/latest/jre/bin/javaws
-rm -rf /usr/java/latest/jre/lib/javaws.jar
-rm -rf /usr/java/latest/jre/lib/desktop
-rm -rf /usr/java/latest/jre/plugin/
-rm -rf /usr/java/latest/jre/lib/deploy*
-rm -rf /usr/java/latest/jre/lib/*javafx*
-rm -rf /usr/java/latest/jre/lib/*jfx*
-rm -rf /usr/java/latest/jre/lib/amd64/libdecora_sse.so
-rm -rf /usr/java/latest/jre/lib/amd64/libprism_*.so
-rm -rf /usr/java/latest/jre/lib/amd64/libfxplugins.so
-rm -rf /usr/java/latest/jre/lib/amd64/libglass.so
-rm -rf /usr/java/latest/jre/lib/amd64/libgstreamer-lite.so
-rm -rf /usr/java/latest/jre/lib/amd64/libjavafx*.so
-rm -rf /usr/java/latest/jre/lib/amd64/libjfx*.so
-
 echo "Installing Prometheus jmx_exporter"
 JMX_EXPORTER_VERSION="0.3.1"
 wget -q https://repo1.maven.org/maven2/io/prometheus/jmx/jmx_prometheus_javaagent/${JMX_EXPORTER_VERSION}/jmx_prometheus_javaagent-${JMX_EXPORTER_VERSION}.jar
