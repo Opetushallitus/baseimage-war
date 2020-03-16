8eee5a03f58d8bf17255661cc1f33fe5d9daf3a9
Juho Majasaari <juho.majasaari@gofore.com>
2020-03-16 09:43:36 +0200

    Bump glibc to 2.31

--- install.sh
+++ install.sh
@@ -38,7 +38,7 @@ pip3 install \
 rm -rf /root/.cache
 
 echo "Downloading glibc for compiling locale definitions"
-GLIBC_VERSION="2.30-r0"
+GLIBC_VERSION="2.31-r0"
 wget -q -O /etc/apk/keys/sgerrand.rsa.pub https://alpine-pkgs.sgerrand.com/sgerrand.rsa.pub
 wget -q https://github.com/sgerrand/alpine-pkg-glibc/releases/download/${GLIBC_VERSION}/glibc-${GLIBC_VERSION}.apk
 wget -q https://github.com/sgerrand/alpine-pkg-glibc/releases/download/${GLIBC_VERSION}/glibc-bin-${GLIBC_VERSION}.apk
